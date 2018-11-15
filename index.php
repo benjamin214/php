de55c3686be3d0130c1512d9bd18fa530b7c93a2
unknown <bbenjamin214@gmail.com>
2018-11-15 16:24:25 +0100

    first

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
+<!DOCTYPE html>
 <html>
 <head>
-
-<header>
- <h2>Friend book</h2>
-</header>
-
-
-
-<style>
-
+	<title>Friend book</title>
+	<style>
 /* Style the header */
 header {
  background-color: #666;
@@ -24,36 +18,59 @@ footer {
  text-align: center;
  color: white;
 }
+p{
+	font-size: 35px;
+
+}
+li{
+	width: 10;
+}
 </style>
 </head>
 
-
+</head>
 <body>
+<header>
+ <h2>Friend book</h2>
+</header>
+<br>
+	<form method="post" action="index.php" >
+	 Name:<input type="text" name="name">
+	<input type="submit">
+	</form>
+<br>
 
-<form action="index.php" method="post">
-Name: <input type="text" name="name">
-<input type="submit">
-</form>
-
+<p><b>My Best Friends :</b></p>
 <?php
-echo "<h1>My best friend: </h1>";
-if (isset($_POST['name'])) {
- $name = $_POST['name'];
-}
-$filename = 'newfile.txt';
-echo file_get_contents($filename);
-$file = fopen( $filename, "a+" );
-if( $file != false ) {
- echo "<b>$name</b><br>";
- fwrite( $file, "$name\n" );
- fclose( $file );
-}
+	$filename = 'friends.txt';
+	if(isset($_POST['name'])) {
+		$file = fopen( $filename, "a" );
+		fwrite( $file, $_POST['name']);
+		fwrite( $file, "\n");
+	}
 
+	$file = fopen( $filename, "r" );
+	while (!feof($file)) {
+	    $ligne=fgets($file);
+	    if(isset($_POST['filter']) and $_POST['filter']!=''){
+	    	$ligne=strstr($ligne, $_POST['filter'],true).strstr($ligne, $_POST['filter']);
+	    	if($ligne)echo "<ul><li>$ligne</ul></li><br>";
+	    }
+	    else{
+	    	if($ligne)echo "<ul><li>$ligne</ul></li><br>";
+	    }
+	    
+	}
+	
 ?>
-
-</body>
-
+<br>
+	<form method="post" action="index.php" >
+	<input type="text" name="filter">
+	<input type="submit" value="Filter list">
+	</form>
+<br>
 <footer>
- <p>Footer</p>
+ <h2>footer</h2>
 </footer>
+</body>
 </html>
